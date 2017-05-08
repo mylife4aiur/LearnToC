335e785d5d43a0883cf8f5d869070b0b76a3969c
Jason Yang <hao_yu_yang@outlook.com>
2017-05-08 09:42:55 +0800

    first commit

---
+++ FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cpp
@@ -0,0 +1,13 @@
+// FirstConsoleApp.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>  
+using namespace std;
+
+int main()
+{
+	cout << "Hello\n";
+    return 0;
+}
+
---
+++ FirstConsoleApp/enc_temp_folder/65313a785a4ee14dd38e42a293f9dc34/s.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include <iostream>  
+#include<unordered_map>
+using namespace std;
+
+int main()
+{
+	cout << (8>>0);
+	cout << (0<<8);
+	return 0;
+}
+
+class Solution {
+public:
+	bool DFS_win(unordered_map<int, bool> &status, int current, int &max_choose, int total)
+	{
+		if (total <= 0)
+			return false;
+		//hashmap中如果存在current(之前访问过)，就直接问返回对应的值
+		if (status.find(current) != status.end())
+			return status[current];
+		// 递归循环
+		for (int i = 1; i <= max_choose; i++)
+		{
+			if (current == (current | 1 << (i - 1)))
+				continue;
+			if (!DFS_win(status, current | 1 << (i - 1), max_choose, total - i))
+			{
+				status[current] = true;
+				return true;
+			}
+		}
+		status[current] = false;
+		return status[current];
+	}
+	bool canIWin(int maxChoosableInteger, int desiredTotal) {
+		if ((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal)
+			return false;
+		if (desiredTotal <= 0)
+			return true;
+		unordered_map<int, bool> status;
+		int current = 0;
+		return DFS_win(status, current, maxChoosableInteger, desiredTotal);
+
+	}
+};
